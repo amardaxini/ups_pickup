93badc4b28f048a318e58e4bfb2475209354c4bb
amardaxini <amardaxini@gmail.com>
2013-03-14 13:00:32 +0530

    added some commet

--- lib/ups_pickup/pickup_piece.rb
+++ lib/ups_pickup/pickup_piece.rb
@@ -1,6 +1,9 @@
 module UpsPickup
   class PickupPiece
-    #attr_accessor :service_code
+    # attr_accessor :service_code
+    # SERVICE CODE :001 Next Day AIR
+    # 002 Ups 2 day air,003 ups ground,012 -3 day select,013 -ups next day air saver
+    # 01 = PACKAGE 02 = UPS LETTER  03 = PALLET 03 is deprecated from UPS
     def initialize(options={})
       @service_code = options[:service_code] || "001"
       @quantity =options[:quantity] || "1"
@@ -14,7 +17,7 @@ module UpsPickup
         "ns2:Quantity" =>@quantity,
         "ns2:DestinationCountryCode"=>@destination_country_code,
         "ns2:ContainerCode"=>@container_code
-      }  
+      }
     end
   end
 end
