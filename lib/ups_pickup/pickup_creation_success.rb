fa0bc88ed849b0771631f05b529c1ec3f5bdbc3e
Alexey S. Brovikov <alexey.brovikov@gmail.com>
2015-12-25 17:49:43 +0700

    Remove unused :total accessor

--- lib/ups_pickup/pickup_creation_success.rb
+++ lib/ups_pickup/pickup_creation_success.rb
@@ -2,7 +2,7 @@ require 'ups_pickup/util'
 module UpsPickup
   class PickupCreationSuccess
     include Util
-    attr_accessor :prn,:rate_status,:success_code, :total, :grand_total_of_all_charge
+    attr_accessor :prn,:rate_status,:success_code, :grand_total_of_all_charge
     def initialize(response)
       response_hash = response.to_hash
       @prn = response_hash.deep_fetch(:prn)
